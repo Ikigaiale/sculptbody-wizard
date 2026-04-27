6d58216c5cfe6efeb596794c832ec438435e8606
Ikigaiale <alejandraquijadab@gmail.com>
2026-04-27 14:11:49 -0400

    Create _branches.js

---
+++ netlify/functions/_branches.js
@@ -0,0 +1,7 @@
+module.exports = [
+  { nombre: 'La Dehesa',   user: '7kqmjjngo',  pass: '41m7ggdejb0813u1g03w6itz27dr278jxzuu1tirk61umd9', url: 'https://sculptbodyladehesa.site.agendapro.com/cl/sucursal/392853' },
+  { nombre: 'Las Condes',  user: 'h9ki8rg6',    pass: 'f2rpbfw0x3p13nflmdfmrif4fe3f0r3oo23lgxrgtm',       url: 'https://sculptbody.site.agendapro.com/cl/sucursal/25115' },
+  { nombre: 'El Golf',     user: '3er0brdmo',   pass: 'gjdyvlew2iljhrsqg3z3x5afk1rpxu3gdxx1ws6wkh',       url: 'https://sculptbodyelgolf.site.agendapro.com/cl/sucursal/170206' },
+  { nombre: 'San Miguel',  user: '2ddclkg60',   pass: 'z4r41vrksjrsj53ueinu8i8ygcw4qmoz8ydwbrn700mizksx', url: 'https://sculptbodysanmiguel.site.agendapro.com/cl/sucursal/115068' },
+  { nombre: 'Antofagasta', user: '73dr15iec',   pass: '1vmwlx5jfl8pmfbu24i1aqne4sm1lbpc3i0ej13lbc998ov44k', url: 'https://sculptbodyantofagasta.site.agendapro.com/cl/sucursal/361946' },
+];
